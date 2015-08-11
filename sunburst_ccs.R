9599793545f5cf7c3b694a5929f84b24490343b8
j-hagedorn <josh.hagedorn@gmail.com>
2015-08-11 15:26:15 -0400

    Update Rmd file

--- sunburst_ccs.R
+++ sunburst_ccs.R
@@ -3,9 +3,21 @@
 library(magrittr)
 library(sunburstR)
 
+tst2 <- read.csv("https://gist.githubusercontent.com/kerryrodden/7090426/raw/34749be0e1d9dc31ce4ec5be95e61c7380925608/visit-sequences.csv",
+                header = F)
+
+tst <- freqItemsets
+tst$items <- gsub("{|}| ","",tst$items, perl=T)
+tst$items <- gsub(",","-",tst$items, perl=T)
+tst$items <- tolower(tst$items)
+
+tst$patients <- tst$support * pCount
+
+tst %<>% select(items, patients)
+
 freqItemsets %>% 
   
-  sunburst()
+  sunburst(tst)
 
 # remove brackets
 
